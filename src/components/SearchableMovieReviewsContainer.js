7c0176601cd920c7dc798ec06a8bdb41296f342b
github username <github email address>
2021-10-18 20:14:57 -0400

    commit tree d67130c1c2fd010ff30451bb4afb59576f34b5e5

--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -2,8 +2,42 @@ import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
-const NYT_API_KEY = 'your-key-here';
-const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
-            + `api-key=${NYT_API_KEY}`;
+const NYT_API_KEY = 'LiTFsyywOwTAoZIUju3rX24pGhzWm6Vh';
+const BASE_URL =
+  'https://api.nytimes.com/svc/movies/v2/reviews/search.json?' +
+  `api-key=${NYT_API_KEY}&query=`;
+
 
 // Code SearchableMovieReviewsContainer Here
+export default class SearchableMovieReviewsContainer extends Component{
+
+    state={
+      searchTerm: "",
+      reviews: []
+    }
+
+    fetchSearch= (searchTerm) => {
+        
+      } 
+
+      handleSubmit = e => {
+        e.preventDefault()
+        fetch(BASE_URL.concat(this.state.searchTerm))
+        .then(response => response.json())
+        .then(response=> this.setState({reviews: response.results}))
+      }
+
+      handleSearchInputChange = event => this.setState({ searchTerm: event.target.value });
+      
+
+    render (){
+        return <div className="searchable-movie-reviews">
+          <form onSubmit= {this.handleSubmit}>
+            <input onChange={this.handleSearchInputChange}></input>
+            {/* why couldnt i do an inline searchTerm? and i had to make it a func*/}
+          </form>
+
+          <MovieReviews reviews= {this.state.reviews} />
+        </div>
+    }
+}
